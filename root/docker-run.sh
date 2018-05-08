34c34428444ced896141a7e36b88caa43f5d3721
Simon Erhardt <hello@rootlogin.ch>
2018-05-08 09:26:49 +0200

    Use # to replace URL

--- root/docker-run.sh
+++ root/docker-run.sh
@@ -13,7 +13,7 @@ fi
 chown -R squid:squid /cache
 chown -R squid:squid /logs
 
-sed -i "s,http://exampleblockpage.com,${REDIRECT_URL},g" /usr/local/squidGuard/squidGuard.conf
+sed -i "s#http://exampleblockpage.com#${REDIRECT_URL}#g" /usr/local/squidGuard/squidGuard.conf
 
 if [ ! -d /cache/00 ]; then
   echo "Initializing cache..."
